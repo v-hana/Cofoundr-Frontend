e1acb648ff708e68c29aa6425c6331c741e1be7a
hana <vhana8844@gmail.com>
2025-01-15 12:29:09 +0530

    Login Frontend updated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -124,9 +124,9 @@ const LoginForm = () => {
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Logo_2013_Google.png"
               alt="Google"
-              className="h-5"
+              className="h-5 "
             />
-            <span>Continue with Google</span>
+            
           </button>
         </div>
       </div>
