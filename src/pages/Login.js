0ad3e824e225aa482f9c690464c2745827acf448
Fathima-Fidha <fathimafidha2903@gmail.com>
2025-01-16 10:11:57 +0530

    Login Updated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
+
 import { Pagination } from "swiper/modules";
 
 
