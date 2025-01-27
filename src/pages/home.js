1248737cd4a76b13fc872567eb74983ca1fb69eb
hana <vhana8844@gmail.com>
2025-01-27 12:47:55 +0530

    added homepage

---
+++ src/pages/home.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import PostCard from "../components/PostCard";
+
+const Home = () => {
+  const posts = [
+    {
+      name: "John Anderson",
+      date: "1/2/2034",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu quam, placerat sed sollicitudin...",
+      image: "https://i.ibb.co/7nYvTwx/medium-shot-man-wearing-vr-glasses.jpg",
+    },
+    {
+      name: "Jane Doe",
+      date: "3/5/2034",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu quam, placerat sed sollicitudin...",
+      image: "https://i.ibb.co/7nYvTwx/medium-shot-man-wearing-vr-glasses.jpg",
+    },
+  ];
+
+  return (
+    <div className="bg-[#2D2638] min-h-screen">
+      {/* Navbar */}
+      <Navbar />
+      <hr className="border-y border-[#3B364C] h-px" />
+
+      {/* Posts */}
+      <div className="px-6 md:px-20 mx-auto space-y-6 mt-6">
+        {posts.map((post, index) => (
+          <PostCard
+            key={index}
+            name={post.name}
+            date={post.date}
+            content={post.content}
+            image={post.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
